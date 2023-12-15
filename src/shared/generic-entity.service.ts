65b402af02c55152baabc886eb23e89ba3c9bf9a
Radoslaw Bura <radoslaw.bura@gmail.com>
2023-12-15 13:53:05 +0100

    feat: add generic entity service to avoid redundant code in all the services

---
+++ src/shared/generic-entity.service.ts
@@ -0,0 +1,92 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable, Logger } from '@nestjs/common';
+import {
+  ApiResponse,
+  FormattedApiResponse,
+  TimestampsAndIdentifier,
+  TimestampsAndUrl,
+} from '../common/types/api-response.type';
+import { catchError, firstValueFrom, map } from 'rxjs';
+import { AxiosError } from 'axios';
+import { getIdFromUrl } from '../common/utilities/get-id-from-url.utility';
+
+@Injectable()
+export class GenericEntityService<T> {
+  private readonly logger = new Logger(this.entityName);
+
+  constructor(
+    protected readonly httpService: HttpService,
+    private readonly entityName: string,
+  ) {}
+
+  async getAll(skip: number, take: number): Promise<FormattedApiResponse<T>> {
+    const pageSize = 10;
+    const totalRequestedItems = take;
+    let remainingItems = totalRequestedItems;
+
+    const results: (T & TimestampsAndIdentifier)[] = [];
+    const resourceTotalNumber: number = 0;
+
+    while (remainingItems > 0) {
+      const response = await firstValueFrom(
+        this.httpService
+          .get<ApiResponse<T & TimestampsAndUrl>>(
+            `https://swapi.dev/api/${this.entityName}/?page=${Math.ceil(
+              (skip + 1) / pageSize,
+            )}`,
+          )
+          .pipe(
+            catchError((error: AxiosError) => {
+              this.logger.error(error.response.data);
+              throw `An error happened while fetching ${this.entityName}!`;
+            }),
+          ),
+      );
+
+      const fetchedItems = response.data.results
+        .slice(skip % pageSize, (skip % pageSize) + remainingItems)
+        .map((item) => ({
+          ...item,
+          id: parseInt(getIdFromUrl(item.url)),
+        }));
+
+      results.push(...fetchedItems);
+
+      remainingItems -= fetchedItems.length;
+      skip += fetchedItems.length;
+
+      if (fetchedItems.length === 0) {
+        break;
+      }
+    }
+
+    const apiResponse: FormattedApiResponse<T> = {
+      results,
+      take,
+      skip,
+      total: resourceTotalNumber,
+    };
+
+    return apiResponse;
+  }
+
+  async getById(id: number): Promise<T> {
+    return await firstValueFrom(
+      this.httpService
+        .get<any>(`https://swapi.dev/api/${this.entityName}/${id}`)
+        .pipe(
+          map((axiosResponse) => {
+            const formattedResponse: T & TimestampsAndIdentifier = {
+              ...axiosResponse.data,
+              id: parseInt(getIdFromUrl(axiosResponse.data.url)),
+            };
+            return formattedResponse;
+          }),
+          catchError((error: AxiosError) => {
+            this.logger.error(error.response.data);
+            throw `An error happened while fetching ${this.entityName}!`;
+          }),
+        ),
+    );
+  }
+}
